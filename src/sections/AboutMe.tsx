53fb370304850d524de89b23cfe6d773861dfcd1
André Luiz J Poles <andreluizjunqpoles@gmail.com>
2025-11-29 19:51:06 -0300

    fix: correct import path for Card component and add missing imageAlt prop in AboutMe section

--- src/sections/AboutMe.tsx
+++ src/sections/AboutMe.tsx
@@ -1,9 +1,9 @@
-import { Card } from "../components/card/Card"
+import { Card } from "../components/Card/Card"
 
 
 export function AboutMe() {
   return (
-    <Card image="https://placehold.co/600x400">
+    <Card image="https://placehold.co/600x400" imageAlt="Foto da Psicóloga Amanda Junqueira Poles">
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec est eleifend, fermentum dolor nec, tempor eros. Phasellus fermentum commodo imperdiet. Praesent sed sollicitudin libero. Fusce finibus maximus vulputate. Aenean ut neque aliquet nisi vestibulum sagittis. Etiam ac tortor vel mi tristique convallis sit amet a nisl. Maecenas varius tincidunt enim, id congue turpis convallis lobortis. Pellentesque scelerisque eu dui vitae semper. Etiam sapien sem, venenatis at sem tincidunt, porttitor pretium est. Pellentesque in metus scelerisque, vulputate sem non, congue orci. Curabitur at interdum leo. Sed nisi diam, pulvinar eget velit non, ullamcorper pulvinar magna.
       Ut ultricies aliquam mi in porta. Nulla facilisi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis cursus lacus arcu, vel molestie dolor sollicitudin a. Nullam vulputate orci erat, quis hendrerit lorem faucibus eu. Sed vel aliquam tellus. Aenean tempor vestibulum eros, vel lacinia erat ullamcorper tempor. Sed gravida libero in velit venenatis egestas.
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec est eleifend, fermentum dolor nec, tempor eros. Phasellus fermentum commodo imperdiet. Praesent sed sollicitudin libero.
